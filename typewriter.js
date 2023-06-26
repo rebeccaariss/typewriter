c5960cd1ef0139ccab15093b69e1d5073fb14cb3
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-26 16:18:04 -0400

    Print letters in string individually

--- typewriter.js
+++ typewriter.js
@@ -1,8 +1,13 @@
 const sentence = "hello there from lighthouse labs";
+let time = 0;
 
 for (const char of sentence) {
   // process.stdout.write gives us more control than console.log
   // (prints individual characters one by one on single line, rather
   // than adding \n after every character as console.log does):
-  process.stdout.write(char);
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, time);
+
+  time += 50;
 }
