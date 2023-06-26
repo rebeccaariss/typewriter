8983b4239b83b169b3550ebc0dc1e05a23307cd7
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-06-26 16:26:58 -0400

    Add newline character to fix bug in output; take command line argument to make function reusable

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,20 @@
-const sentence = "hello there from lighthouse labs";
-let time = 0;
+const str = process.argv[2];
 
-for (const char of sentence) {
-  // process.stdout.write gives us more control than console.log
-  // (prints individual characters one by one on single line, rather
-  // than adding \n after every character as console.log does):
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, time);
+const typewriter = function(str) {
+  let sentence = str;
+  sentence += "\n";
+  let time = 0;
+  
+  for (const char of sentence) {
+    // process.stdout.write gives us more control than console.log
+    // (prints individual characters one by one on single line, rather
+    // than adding \n after every character as console.log does):
+    setTimeout(() => {
+      process.stdout.write(char);
+    }, time);
+  
+    time += 50;
+  }
+};
 
-  time += 50;
-}
+typewriter(str);
